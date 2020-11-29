cb68cc1e5f904a0e669c668d3fa2fe17c4de91b5
fajrianfatan <fajrianfatan16@gmail.com>
2020-11-29 21:46:43 +0700

    Resep Sajiku

---
+++ app/src/main/java/com/example/resepsajiku/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.resepsajiku;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+
+    String s1[], s2[];
+    int images[] = {R.drawable.ayamgoreng,R.drawable.bihunkuah,R.drawable.bakwantahu,R.drawable.sayurbayam,R.drawable.telurbalado};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+
+        s1 = getResources().getStringArray(R.array.resep_masak);
+        s2 = getResources().getStringArray(R.array.description);
+
+        MyAdapter myAdapter = new MyAdapter(this, s1, s2, images);
+        recyclerView.setAdapter(myAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+}
